e960daecd17aad267cccd3d1591d27bfe4b34888
jhosep98 <jhosep149@hotmail.com>
2024-11-20 11:59:36 -0300

    feat: textHost production

--- src/components/bewomo-cta.tsx
+++ src/components/bewomo-cta.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import { CTAWidget } from "@wulperstudio/cta";
 
 export const BewomoCta: React.FC = () => {
+  console.log(window.location.href)
+
   return (
     <CTAWidget
       preference="81fab0d1-6542-43db-8af7-8fd41e868dd7"
