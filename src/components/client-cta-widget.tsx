29fc9828881d3c2da5c9a28f33157b4c279653fc
jhosep98 <jhosep149@hotmail.com>
2025-04-11 11:04:04 -0300

    fx: mode test with id_handler

--- src/components/client-cta-widget.tsx
+++ src/components/client-cta-widget.tsx
@@ -17,9 +17,9 @@ export const ClientCtaWidget: React.FC = () => {
   return (
     <CTAWidget
       preference="7a3c3f11-6e88-4191-af1b-1b6ae58a434b"
-      mode="production"
+      mode="test"
       id_handler="cta_handler"
-      // testHost="https://real-estate-landing-tau.vercel.app/"
+      testHost={"https://real-estate-landing-tau.vercel.app/"}
       // testHost={"https://real-estate-landing-tau.vercel.app/"}
     />
   );
